814abe8207ffc7f0b939a08704b4d2a2047c822f
I31939 <gunasekhar.koppanati@verisk.com>
2023-07-07 16:16:40 -0400

    Init

---
+++ OOPS.APP/Encapsulation.App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Encapsulation.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var taxCalculatorInstance = new TaxCalculator();
+            var njLoanAmountAfterOneYear = taxCalculatorInstance.LoanWithIntrestRate(100, "NJ", 1);
+            Console.WriteLine($"Loan Amount NJ : {njLoanAmountAfterOneYear}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOPS.APP/Encapsulation.App/TaxCalculator.cs
@@ -0,0 +1,34 @@
+﻿namespace Encapsulation.App
+{
+    /// <summary>
+    ///  Encapsulation, is to make sure that "sensitive" data is hidden from users
+    /// </summary>
+    public class TaxCalculator
+    {
+        private readonly double NJ = 6.5;
+        private readonly double NY = 10;
+        // Write Code Here
+
+        public double LoanWithIntrestRate(double amount, string state, int years)
+        {
+            double result = 0;
+            switch (state)
+            {
+                case "NJ":
+                    result = amount + SimpleIntrest(amount, NJ, years);
+                    break;
+                case "NY":
+                    result = amount + SimpleIntrest(amount, NY, years);
+                    break;
+                    // Write Code Here
+            }
+            return result;
+        }
+
+        public double SimpleIntrest(double principal, double intrestRate, int years)
+        {
+            return principal * intrestRate * years / 100;
+        }
+
+    }
+}
