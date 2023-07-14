c5dcc20e65f9f0d36b5fa35862fdb5e4bb3418fa
I31939 <gunasekhar.koppanati@verisk.com>
2023-07-14 14:56:59 -0400

    interface

---
+++ OOPS.APP/Abstraction.App/Interfaces/ICar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstraction.App.Interfaces
+{
+     interface ICar
+    {
+        void CarModel();
+        void Year();
+    }
+}
---
+++ OOPS.APP/Abstraction.App/Interfaces/Tesla.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Abstraction.App.Interfaces
+{
+    public class Tesla : ICar
+    {
+        public void CarModel()
+        {
+            Console.WriteLine("Tesla Model S with 'ICar' interface.");
+        }
+
+        public void Year()
+        {
+            Console.WriteLine($"Tesla Car Year : {DateTime.Now.Year} with 'ICar' interface.");
+        }
+    }
+}
